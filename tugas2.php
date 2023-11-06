c3b066863bd7607a702861fcc2e35cf0b293abb5
Farel Habibillah <133088011+yuuuuiiiii@users.noreply.github.com>
2023-11-06 08:25:12 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,119 @@
+<?php
+
+$floraFaunaTerancam = array(
+    "Spesies 1" => array(
+        "Nama" => "Harimau Sumatera",
+        "Penjelasan" => "Harimau Sumatera adalah subspesies harimau yang hanya ditemukan di pulau Sumatera, Indonesia. Populasinya sangat terancam punah karena perburuan dan hilangnya habitat.",
+        "Jumlah Populasi" => "Kira-kira 400 ekor",
+        "Daerah" => "Sumatera, Indonesia",
+        "Gambar" => "harimau2.jpg"
+    ),
+
+    "Spesies 2" => array(
+        "Nama" => "Kakatua Raja",
+        "Penjelasan" => "Kakatua Raja adalah spesies burung yang ditemukan di Indonesia, terutama di Papua. Mereka terancam punah akibat perdagangan ilegal dan hilangnya habitat.",
+        "Jumlah Populasi" => " 6,000 individu",
+        "Daerah" => "Papua, Indonesia",
+        "Gambar" => "kakatua+raja.jpg"
+    ),
+
+    "Spesies 3" => array(
+        "Nama" => "Rafflesia arnoldi",
+        "Penjelasan" => "Raflesia arnoldi adalah tanaman dengan bunga tunggal terbesar di dunia yang berasal dari Bengkulu. Agus Susatya dalam buku Rafflesia Pesona Bunga Terbesar di Dunia (2011) menyebutkan Raflesia arnoldi mempunyai rata-rata 10 kuncup per populasi dengan mortalitas (tingkat kematian) 20 persen hingga 100 persen. Dengan tingkat mortalitas yang tinggi juga reporduksi yang sulit membuat bunga raflesia arnoldi masuk ke dalam daftar merah flora terancam punah menurut International Union for Conservation of Nature (IUCN).",
+        "Jumlah Populasi" => "Kira-kira 16 yang mekar",
+        "Daerah" => "Bengkulu ",
+        "Gambar" => "rafllesia arnoldi.jpg",
+    ),
+
+    "Spesies 4" => array(
+        "Nama" => "Bunga bangkai",
+        "Penjelasan" => "Bunga bangkai adalah bunga yang memiliki ciri khas beraroma busuk, seperti bau bangkai. Aroma busuknya, membuat bunga bangkai ini menjadi sangat berbeda dengan kebanyakan jenis bunga lainnya yang memiliki aroma wangi. Selain itu, bungai bangkai memiliki tubuh tumbuhan yang sangat besar jika dibandingkan dengan beragam jenis bunga lainnya, bahkan disebut-sebut sebagai bunga tertinggi di dunia.",
+        "Jumlah Populasi" => " 25 yang mekar",
+        "Daerah" => "Bengkulu",
+        "Gambar" => "bunga-bangkai.webp"
+
+    ),
+
+    "Spesies 5" => array(
+        "Nama" => "Komodo",
+        "Penjelasan" => "Komodo adalah spesies kadal raksasa yang hanya ditemukan di pulau Komodo, Indonesia. Mereka menghadapi ancaman dari perburuan ilegal dan perusakan habitat.",
+        "Jumlah Populasi" => "5.000 ekor",
+        "Daerah" => "Pulau Komodo, Indonesia",
+        "Gambar" => "komodo.jpg"
+    ),
+
+    "Spesies 6" => array(
+        "Nama" => "Badak Sumatera",
+        "Penjelasan" => "Badak Sumatera adalah subspesies badak yang hanya ditemukan di Sumatera, Indonesia. Populasi mereka sangat terancam punah karena perburuan ilegal dan perusakan habitat.",
+        "Jumlah Populasi" => "100 ekor",
+        "Daerah" => "Sumatera, Indonesia",
+        "Gambar" => "badak sumatera.jpeg"
+    ),
+
+    "Spesies 7" => array(
+        "Nama" => "Orangutan Kalimantan",
+        "Penjelasan" => "Orangutan Kalimantan adalah spesies orangutan yang tinggal di Kalimantan, Indonesia. Mereka menghadapi ancaman habitat yang terus berkurang dan perburuan ilegal.",
+        "Jumlah Populasi" => "20.000 ekor",
+        "Daerah" => "Kalimantan, Indonesia",
+        "Gambar" => "orang-utan.jpg"
+    ),
+
+    "Spesies 8" => array(
+        "Nama" => "Merak Hijau",
+        "Penjelasan" => "Merak hijau (Pavo muticus) adalah salah satu burung dari tiga spesies merak. Seperti burung-burung lainnya yang ditemukan di suku Phasianidae, merak hijau mempunyai bulu yang indah. Bulu-bulunya berwarna hijau keemasan. Burung jantan dewasa berukuran sangat besar, panjangnya dapat mencapai 300 cm, dengan penutup ekor yang sangat panjang. Di atas kepalanya terdapat jambul tegak. Burung betina berukuran lebih kecil dari burung jantan. Bulu-bulunya kurang mengilap, berwarna hijau keabu-abuan, dan tanpa dihiasi bulu penutup ekor.",
+        "Jumlah Populasi" => "sekitar 3.000 populasi",
+        "Daerah" => "Jawa",
+        "Gambar" => "merak-hijau.jpg"
+    ),
+
+    "Spesies 9" => array(
+        "Nama" => "Penyu Hijau",
+        "Penjelasan" => "Penyu Hijau adalah spesies penyu yang berkembang biak di pantai-pantai Indonesia. Mereka terancam punah karena perburuan telur dan hilangnya habitat pesisir.",
+        "Jumlah Populasi" => "200,000 ekor",
+        "Daerah" => "Pantai-pantai Indonesia",
+        "Gambar" => "penyu hijau.JPG"
+    ),
+    "Spesies 10" => array(
+        "Nama" => "Burung cendrawasih",
+        "Penjelasan" => "Burung Cenderawasih adalah anggota famili Paradisaeidae dari ordo Passeriformes. Cenderawasih biasanya ditemukan di Indonesia seperti di bagian Timur Papua, Papua Nugini, pulau-pulau selat Torres, dan Australia timur. Burung anggota keluarga ini dikenal karena bulu burung jantan pada banyak jenisnya, terutama bulu yang sangat memanjang dan rumit yang tumbuh dari paruh, sayap atau kepalanya. Ukuran burung Cenderawasih mulai dari Cenderawasih raja pada 50 gram dan 15 cm hingga Cenderawasih paruh-sabit Hitam pada 110 cm dan Cenderawasih manukod jambul-bergulung pada 430 gram.",
+        "Jumlah Populasi" => "28 ekor ",
+        "Daerah" => "Papua dan australia",
+        "Gambar" => "burung cendrawasih.jpg"
+    ),
+
+);
+
+echo "<table border='1'>";
+echo "<tr><th>No</th><th>Nama</th><th>Penjelasan</th><th>Jumlah Populasi</th><th>Daerah</th><th>Gambar</th></tr>";
+$nomor = 1;
+foreach ($floraFaunaTerancam as $spesies) {
+    echo "<tr>";
+    echo "<td>$nomor</td>";
+    echo "<td>" . $spesies["Nama"] . "</td>";
+    echo "<td>" . $spesies["Penjelasan"] . "</td>";
+    echo "<td>" . $spesies["Jumlah Populasi"] . "</td>";
+    echo "<td>" . $spesies["Daerah"] . "</td>";
+    echo "<td><img src='" . $spesies["Gambar"] . "' width='100'></td>";
+    echo "</tr>";
+    $nomor++;
+}
+echo "</table>";
+?>
+
+
+
+
+
+
+
+
+    
+    
+
+
+    
+
+    
+
+    
+
